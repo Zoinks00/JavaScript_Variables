30b3993c871fdbc839437ae39c8737d0693bf1ab
Forrest Rabideau <forrest.rabideau@gmail.com>
2022-05-18 17:39:09 -0700

    1st commit

--- app.js
+++ app.js
@@ -4,7 +4,19 @@ console.log(
 );
 
 // Exercise 1
-
+const firstName = "Forrest";
+console.log("FirstName:", firstName);
+let lastName = "Rabideau";
+console.log("LastName:", lastName);
+var age = 37;
+console.log("Age:", age);
 // Exercise 2
-
+let fullName = firstName+" "+lastName;
+console.log(fullName);
 // Exercise 3
+let city = "Hillsboro";
+let pastime = "spending time with the family";
+let myStory; 
+myStory = `Hello! My name is ${fullName} and I am around ${age} years old. I currently live in ${city}, Oregon with wife and stepson. When not at work I enjoy ${pastime}.`;
+console.log(myStory);  
+          
