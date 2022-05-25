1b0c51615957bced8bc31127f340bd066a4f5b53
Forrest Rabideau <forrest.rabideau@gmail.com>
2022-05-25 00:56:39 -0700

    1st commit

--- app.js
+++ app.js
@@ -19,4 +19,27 @@ let pastime = "spending time with the family";
 let myStory; 
 myStory = `Hello! My name is ${fullName} and I am around ${age} years old. I currently live in ${city}, Oregon with wife and stepson. When not at work I enjoy ${pastime}.`;
 console.log(myStory);  
-          
+ //above script is from class handout
+ //below script from README file
+ let language = "JavaScript";
+ let createYear = 1995;
+ let isCaseSensitive = true;
+ console.log(`${language} was created in ${createYear} and is casesenitive ${isCaseSensitive}.`); 
+ //Exercise 4
+ let price = 19.99;
+ let isOnSale = false;
+ let salePercentage = 15;
+ let stock = 0;
+ let inStock = false;
+ let selectedSize = "M";
+ console.log(`Shirt size ${selectedSize} cost ${price} with ${stock} in stock. ${salePercentage}'%' sale price applied: ${isOnSale}.`);
+
+ //Exercise 5
+ let title = "Name of the Wind";
+ let author = "Patrick Rothfuss";
+ let pageCount = 722;
+ let bookmark = 456;
+ let hasRead = true;
+ console.log(`Book title: ${title} by author ${author} has ${pageCount} pages.`);
+ console.log (`Timmy has read: ${hasRead} up to ${bookmark} pages of ${title}.`);
+ 
